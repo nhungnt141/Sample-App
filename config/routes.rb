f7bdcad977a65fd0ebfd858f115c03cd9e3d81c8
NhungNT <nhungnt141@gmail.com>
2020-12-08 21:50:59 +0700

    Chap 07

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  resources :users
+  get "/users/:id", to: "users#show"
+  get '/users', to: "users#index"
+  # resources: users
 end
