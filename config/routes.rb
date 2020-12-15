577f116464b53b2a3f83cecceb396cf1eda8943e
NhungNT <nhungnt141@gmail.com>
2020-12-15 21:35:59 +0700

    Implement basic login chap 08

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
@@ -7,5 +8,8 @@ Rails.application.routes.draw do
   get 'users/new', to: 'users#new'
   # get "/users/:id", to: "users#show"
   # post "/users", to: "users#index"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
